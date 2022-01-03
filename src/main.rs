ea4aec4c5fa297a4148c8c6aa144df69ffb82041
Justin Jaffray <justin.jaffray@gmail.com>
2022-01-03 12:55:12 -0800

    get the instant in the loop

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::{fs::File, io::Write, time::Instant};
 
 fn main() {
-    let mut t = Instant::now();
     let mut file = File::create("foo").unwrap();
     for _ in 0..10 {
+        let t = Instant::now();
         file.write_all("bar".as_bytes()).unwrap();
         file.sync_all().unwrap();
         println!("{}ms", t.elapsed().as_secs_f64() * 1000.0);
-        t = Instant::now();
     }
 }
